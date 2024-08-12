0b31edd0746e7add417964d6ab0730b397b840fb
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-08-12 22:47:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+hide = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+
 st.title('🚢 Can You Survive the Titanic?')
 st.info('Enter your data below to test!')
