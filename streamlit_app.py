4ed25a67d5325d6104d783fcc7eb20172b3cba15
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-08-13 17:20:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,9 @@ with st.form("my_form"):
                     'Embarked': [embarked[0]]
         }
                 cred_df = pd.DataFrame(cred_dict)
+                st.write(cred_df)
                 creds = preprocess_cred(cred_df)
+                st.write(creds)
                 ypred = model.predict(creds)
                     
                 if ypred[0] == 0:
